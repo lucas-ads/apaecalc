7727ed568d0340e63a83d6f2764184ff0372de4f
lucas-ads <lucas_ads@hotmail.com.br>
2016-12-13 21:00:43 -0300

    Remoção de arquivo desnecessário

--- alterações banco.sql
+++
@@ -1,6 +0,0 @@
-alter table turma add column observacao varchar(100);
-alter table turma change observacao periodo varchar(15);
-alter table estudante add column turma_atual integer unsigned not null;
-ALTER TABLE estudante ADD CONSTRAINT `fk_turma` FOREIGN KEY (turma_atual) REFERENCES turma(id);
-
-alter table partida add column data_partida date not null;
